fc451542d76355323885d8f4ab243b41635c920f
Namoryx <nknn2025@gmail.com>
2025-12-17 19:58:44 +0900

    fix: align gh-pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/neo4j-cyper-game/',
   plugins: [react()],
+  base: '/neo4j-cyper-game/',
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
