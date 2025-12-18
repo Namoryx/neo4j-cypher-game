cebf44f0991946a3c5d9740f090f47fa8c406e67
Namoryx <nknn2025@gmail.com>
2025-12-18 17:39:06 +0900

    chore: switch quokka asset back to svg

--- src/components/QuokkaCharacter.jsx
+++ src/components/QuokkaCharacter.jsx
@@ -1,7 +1,9 @@
 function QuokkaCharacter({ speech, mood = 'ask' }) {
+  const imageSrc = `${import.meta.env.BASE_URL}assets/quokka.svg`;
+
   return (
     <div className={`card quokka quokka--${mood}`} data-testid="quokka">
-      <img src="./assets/quokka.svg" alt="Quokka mascot" />
+      <img src={imageSrc} alt="Quokka mascot" />
       <div className="speech" data-testid="speech">
         {speech}
       </div>
