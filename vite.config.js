e6791941f3a35a3ef7294514d847e7ac5fb75d77
Namoryx <nknn2025@gmail.com>
2025-12-19 20:14:27 +0900

    fix: avoid jsx module load in production

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/neo4j-cyper-game/',
   plugins: [react()],
+  base: '/neo4j-cyper-game/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'assets/chunk-[hash].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/app.css';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
